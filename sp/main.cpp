2794050f31ba59f03ddfd335defbbe45bb3d69d9
Sausty <MikuoH15TH@gmail.com>
2023-01-01 18:35:43 +0100

    [ADD] Added base build system

---
+++ sp/main.cpp
@@ -0,0 +1,11 @@
+/**
+ *  Author: Amélie Heinrich
+ *  Company: Amélie Games
+ *  License: MIT
+ *  Create Time: 01/01/2023 18:18
+ */
+
+int main(int argc, char *argv[])
+{
+    return (0);
+}
---
+++ xmake.lua
@@ -0,0 +1,7 @@
+add_rules("mode.debug", "mode.release")
+
+target("Game Project")
+    add_files("sp/**.cpp")
+    add_headerfiles("sp/**.hpp")
+    add_includedirs("sp", { public = true })
+    -- TODO: Platform detection
