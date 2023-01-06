16565ea05c0246e358cb5c8cdebbaf45ca96ac27
Sausty <MikuoH15TH@gmail.com>
2023-01-06 22:55:27 +0100

    [ADD] Added key events

--- sp/main.cpp
+++ sp/main.cpp
@@ -11,6 +11,7 @@
 #include "timer.h"
 #include "log_system.hpp"
 #include "event_system.hpp"
+#include "input_system.hpp"
 
 struct win32_platform_state
 {
@@ -36,16 +37,56 @@ LRESULT CALLBACK WindowProc(HWND Window, UINT Message, WPARAM WParam, LPARAM LPa
             Data.data.u32[1] = Height;
             EventSystemFire(event_type::Resize, nullptr, Data);
         } break;
+        case WM_KEYDOWN:
+        case WM_SYSKEYDOWN:
+        case WM_KEYUP:
+        case WM_SYSKEYUP:
+        {
+            bool Pressed = (Message == WM_KEYDOWN || Message == WM_SYSKEYDOWN);
+            keyboard_key Key = static_cast<keyboard_key>(WParam);
+            bool IsExtended = (HIWORD(LParam) & KF_EXTENDED) == KF_EXTENDED;
+
+            if (WParam == VK_MENU) 
+                Key = IsExtended ? keyboard_key::RightAlt : keyboard_key::LeftAlt;
+            if (WParam == VK_SHIFT)
+            {
+                uint32_t LeftShift = MapVirtualKey(VK_LSHIFT, MAPVK_VK_TO_VSC);
+                uint32_t ScanCode = ((LParam & (0xFF << 16)) >> 16);
+                Key = ScanCode == LeftShift ? keyboard_key::LeftShift : keyboard_key::RightShift;
+            }
+            if (WParam == VK_CONTROL)
+            {
+                Key = IsExtended ? keyboard_key::RightControl : keyboard_key::LeftControl;
+            }
+
+            event_data Data;
+            Data.data.u16[0] = static_cast<uint16_t>(Key);
+            EventSystemFire(Pressed ? event_type::KeyPressed : event_type::KeyReleased, nullptr, Data);
+        } break;
     }
 
     return DefWindowProc(Window, Message, WParam, LParam);
+}
+
+bool TestEvent(event_type Type, void *Sender, void *ListenerInstance, event_data Data)
+{
+    if (Type == event_type::KeyPressed)
+    {
+        if (Data.data.u16[0] == (uint16_t)keyboard_key::Escape)
+        {
+            LogInfo("Escape Pressed!");
+        }
+    }
+    return true;
 }
 
 int main(int argc, char *argv[])
 {
     game_config_file ConfigFile;
     GameConfigLoad("gameconfig.cfg", &ConfigFile);
-    
+
+    EventSystemInit();
+
     win32_platform_state State = {};
     State.Instance = GetModuleHandle(NULL);
 
@@ -73,5 +114,8 @@ int main(int argc, char *argv[])
 
     DestroyWindow(State.Window);
     UnregisterClassA(State.WindowClass.lpszClassName, State.Instance);
+
+    EventSystemExit();
+    
     return (0);
 }
