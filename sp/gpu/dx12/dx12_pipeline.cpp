be3a97a2d894e7eab53d3ba2a26a62761e687b3e
Sausty <MikuoH15TH@gmail.com>
2023-02-08 12:55:33 +0100

    [ADD] Added D3D12 vertex input layout reflection

--- sp/gpu/dx12/dx12_pipeline.cpp
+++ sp/gpu/dx12/dx12_pipeline.cpp
@@ -14,6 +14,7 @@
 #include "windows/windows_data.hpp"
 
 #include <d3dcompiler.h>
+#include <ranges>
 
 D3D12_FILL_MODE GetDx12FillMode(fill_mode Mode)
 {
@@ -63,22 +64,28 @@ void GpuPipelineCreateGraphics(gpu_pipeline *Pipeline)
     dx12_shader *ShaderPrivate = (dx12_shader*)Pipeline->Info.Shader->Private;
 
     ID3D12ShaderReflection* VertexReflection = nullptr;
+    D3D12_SHADER_DESC VertexDesc;
+    std::vector<D3D12_INPUT_ELEMENT_DESC> InputElementDescs;
+    std::vector<std::string> InputElementSemanticNames;
+
     ID3D12ShaderReflection* PixelReflection = nullptr;
+    D3D12_SHADER_DESC PixelDesc;
 
     HRESULT Result = D3DReflect(ShaderPrivate->VertexBlob->GetBufferPointer(), ShaderPrivate->VertexBlob->GetBufferSize(), IID_PPV_ARGS(&VertexReflection));
     if (FAILED(Result))
         LogError("D3D12: Failed to reflect vertex shader!");
+    VertexReflection->GetDesc(&VertexDesc);
 
     Result = D3DReflect(ShaderPrivate->PixelBlob->GetBufferPointer(), ShaderPrivate->PixelBlob->GetBufferSize(), IID_PPV_ARGS(&VertexReflection));
     if (FAILED(Result))
         LogError("D3D12: Failed to reflect pixel shader!");
+    PixelReflection->GetDesc(&PixelDesc);
 
     D3D12_GRAPHICS_PIPELINE_STATE_DESC Desc = {};
     Desc.VS.pShaderBytecode = ShaderPrivate->VertexBlob->GetBufferPointer();
     Desc.VS.BytecodeLength = ShaderPrivate->VertexBlob->GetBufferSize();
     Desc.PS.pShaderBytecode = ShaderPrivate->PixelBlob->GetBufferPointer();
     Desc.PS.BytecodeLength = ShaderPrivate->PixelBlob->GetBufferSize();
-    
     for (int RTVIndex = 0; RTVIndex < Pipeline->Info.Formats.size(); RTVIndex++)
     {
         Desc.BlendState.RenderTarget[RTVIndex].SrcBlend = D3D12_BLEND_ONE;
@@ -93,13 +100,11 @@ void GpuPipelineCreateGraphics(gpu_pipeline *Pipeline)
         Desc.RTVFormats[RTVIndex] = GetDXGIFormat(Pipeline->Info.Formats[RTVIndex]);
     }
     Desc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;
-
     Desc.RasterizerState.FillMode = GetDx12FillMode(Pipeline->Info.FillMode);
     Desc.RasterizerState.CullMode = GetDx12CullMode(Pipeline->Info.CullMode);
     Desc.RasterizerState.DepthClipEnable = true;
     Desc.RasterizerState.FrontCounterClockwise = true;
     Desc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
-
     if (Pipeline->Info.HasDepth)
     {
         Desc.DepthStencilState.DepthEnable = true;
@@ -108,6 +113,55 @@ void GpuPipelineCreateGraphics(gpu_pipeline *Pipeline)
         Desc.DSVFormat = GetDXGIFormat(Pipeline->Info.DepthFormat);
     }
     Desc.SampleDesc.Count = 1;
+
+    // Reflect input layout
+    InputElementSemanticNames.reserve(VertexDesc.InputParameters);
+    InputElementDescs.reserve(VertexDesc.InputParameters);
+
+    for (int ParameterIndex = 0; ParameterIndex < VertexDesc.InputParameters; ParameterIndex++)
+    {
+        D3D12_SIGNATURE_PARAMETER_DESC ParameterDesc = {};
+        VertexReflection->GetInputParameterDesc(ParameterIndex, &ParameterDesc);
+        
+        InputElementSemanticNames.push_back(ParameterDesc.SemanticName);
+
+        D3D12_INPUT_ELEMENT_DESC InputElement = {};
+        InputElement.SemanticName = InputElementSemanticNames.back().c_str();
+        InputElement.SemanticIndex = ParameterDesc.SemanticIndex;
+        InputElement.InputSlot = 0;
+        InputElement.AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+        InputElement.InputSlotClass = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+        InputElement.InstanceDataStepRate = 0;
+
+        if (ParameterDesc.Mask == 1)
+        {
+            if (ParameterDesc.ComponentType == D3D_REGISTER_COMPONENT_UINT32) InputElement.Format = DXGI_FORMAT_R32_UINT;
+            else if (ParameterDesc.ComponentType == D3D_REGISTER_COMPONENT_SINT32) InputElement.Format = DXGI_FORMAT_R32_SINT;
+            else if (ParameterDesc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32) InputElement.Format = DXGI_FORMAT_R32_FLOAT;
+        }
+        else if (ParameterDesc.Mask <= 3)
+        {
+            if (ParameterDesc.ComponentType == D3D_REGISTER_COMPONENT_UINT32) InputElement.Format = DXGI_FORMAT_R32G32_UINT;
+            else if (ParameterDesc.ComponentType == D3D_REGISTER_COMPONENT_SINT32) InputElement.Format = DXGI_FORMAT_R32G32_SINT;
+            else if (ParameterDesc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32) InputElement.Format = DXGI_FORMAT_R32G32_FLOAT;
+        }
+        else if (ParameterDesc.Mask <= 7)
+        {
+            if (ParameterDesc.ComponentType == D3D_REGISTER_COMPONENT_UINT32) InputElement.Format = DXGI_FORMAT_R32G32B32_UINT;
+            else if (ParameterDesc.ComponentType == D3D_REGISTER_COMPONENT_SINT32) InputElement.Format = DXGI_FORMAT_R32G32B32_SINT;
+            else if (ParameterDesc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32) InputElement.Format = DXGI_FORMAT_R32G32B32_FLOAT;
+        }
+        else if (ParameterDesc.Mask <= 15)
+        {
+            if (ParameterDesc.ComponentType == D3D_REGISTER_COMPONENT_UINT32) InputElement.Format = DXGI_FORMAT_R32G32B32A32_UINT;
+            else if (ParameterDesc.ComponentType == D3D_REGISTER_COMPONENT_SINT32) InputElement.Format = DXGI_FORMAT_R32G32B32A32_SINT;
+            else if (ParameterDesc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32) InputElement.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+        }
+
+        InputElementDescs.push_back(InputElement);
+    }
+    Desc.InputLayout.pInputElementDescs = InputElementDescs.data();
+    Desc.InputLayout.NumElements = static_cast<uint32_t>(InputElementDescs.size());
 
     HRESULT Result = DX12.Device->CreateGraphicsPipelineState(&Desc, IID_PPV_ARGS(&PipelinePrivate->Pipeline));
     if (FAILED(Result)) 
