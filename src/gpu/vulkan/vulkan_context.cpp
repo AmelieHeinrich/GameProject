2d48ee4bfa0d783680585b34d43bae8498f7b4d4
Sausty <MikuoH15TH@gmail.com>
2023-03-13 19:49:44 +0100

    [ADD] Added simple vulkan instance creation

--- src/gpu/vulkan/vulkan_context.cpp
+++ src/gpu/vulkan/vulkan_context.cpp
@@ -8,6 +8,7 @@
 #include "vulkan_context.hpp"
 
 #include "platform_detection.hpp"
+#include "systems/log_system.hpp"
 
 static const char *VulkanInstanceLayers[] = {
     "VK_LAYER_KHRONOS_validation"
@@ -27,7 +28,25 @@ gpu_backend GpuGetBackend()
 
 void GpuInit()
 {
-    // TODO(amelie.h): Initialise instance
+    VkApplicationInfo ApplicationInfo = {};
+    ApplicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    ApplicationInfo.pApplicationName = "Game Project";
+    ApplicationInfo.applicationVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
+    ApplicationInfo.pEngineName = "Cheese Toast Engine";
+    ApplicationInfo.engineVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
+    ApplicationInfo.apiVersion = VK_API_VERSION_1_3;
+
+    // TODO(amelie.h): Pick instance layers and extensions
+
+    VkInstanceCreateInfo CreateInfo = {};
+    CreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    CreateInfo.pApplicationInfo = &ApplicationInfo;
+
+    VkResult Result = vkCreateInstance(&CreateInfo, nullptr, &VK.Instance);
+    if (Result != VK_SUCCESS) {
+        LogError("VULKAN: Failed to create instance!");
+    }
+
     // TODO(amelie.h): Pick physical device
     // TODO(amelie.h): Create logical device
     // TODO(amelie.h): Create command queues
@@ -35,7 +54,7 @@ void GpuInit()
 
 void GpuExit()
 {
-
+    vkDestroyInstance(VK.Instance, nullptr);
 }
 
 void GpuBeginFrame()
